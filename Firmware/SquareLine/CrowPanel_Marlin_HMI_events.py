2dece96c9e96c9019ce03724558ab484748c8886
carl1961 <cmshelton2021@protonmail.com>
2026-05-22 01:03:17 -0500

    updated files
    updated files

---
+++ Firmware/SquareLine/CrowPanel_Marlin_HMI_events.py
@@ -0,0 +1,360 @@
+
+def ui_event_LabelHotendSmall(event_struct):
+    return
+
+
+def ui_event_LabelBedSmall(event_struct):
+    return
+
+
+def ui_event_LabelFilamentMain(event_struct):
+    return
+
+
+def ui_event_BtnShutDown(event_struct):
+    return
+
+
+def ui_eventArcOverall(event_struct):
+    return
+
+
+def ui_event_BarHotend(event_struct):
+    return
+
+
+def ui_eventBtnPauseResume(event_struct):
+    return
+
+
+def ui_eventBtnStop(event_struct):
+    return
+
+
+def ui_eventSliderSpeed(event_struct):
+    return
+
+
+def ui_eventBtnYPlus(event_struct):
+    return
+
+
+def ui_eventBtnYMinus(event_struct):
+    return
+
+
+def ui_eventBtnXMinus(event_struct):
+    return
+
+
+def ui_eventBtnXPlus(event_struct):
+    return
+
+
+def ui_eventBtnZPlus(event_struct):
+    return
+
+
+def ui_eventBtnZMinus(event_struct):
+    return
+
+
+def ui_eventBtnHomeAll(event_struct):
+    return
+
+
+def ui_eventDropdownStep(event_struct):
+    return
+
+
+def ui_eventSliderBabyZ(event_struct):
+    return
+
+
+def ui_eventBtnAutoZAlign(event_struct):
+    return
+
+
+def ui_eventBtnAutoBedLevel(event_struct):
+    return
+
+
+def ui_eventBtnLoadMesh(event_struct):
+    return
+
+
+def ui_eventBtnSaveMesh(event_struct):
+    return
+
+
+def ui_eventBtnXHome(event_struct):
+    return
+
+
+def ui_eventBtnYHome(event_struct):
+    return
+
+
+def ui_eventBtnZHome(event_struct):
+    return
+
+
+def ui_eventSliderXCurrent(event_struct):
+    return
+
+
+def ui_eventDropdownXMicrosteps(event_struct):
+    return
+
+
+def ui_eventSliderXHybrid(event_struct):
+    return
+
+
+def ui_eventSliderXSGT(event_struct):
+    return
+
+
+def ui_eventDropdownXMode(event_struct):
+    return
+
+
+def ui_eventBtnApplyAllTMC(event_struct):
+    return
+
+
+def ui_eventSliderYCurrent(event_struct):
+    return
+
+
+def ui_eventDropdownYMicrosteps(event_struct):
+    return
+
+
+def ui_eventSliderYHybrid(event_struct):
+    return
+
+
+def ui_eventSliderYSGT(event_struct):
+    return
+
+
+def ui_eventDropdownYMode(event_struct):
+    return
+
+
+def ui_eventSliderZ1Current(event_struct):
+    return
+
+
+def ui_eventDropdownZ1Microsteps(event_struct):
+    return
+
+
+def ui_eventSliderZ1Hybrid(event_struct):
+    return
+
+
+def ui_eventSliderZ1SGT(event_struct):
+    return
+
+
+def ui_eventDropdownZ1Mode(event_struct):
+    return
+
+
+def ui_eventSliderZ2Current(event_struct):
+    return
+
+
+def ui_event(event_struct):
+    return
+
+
+def ui_eventSliderZ2Hybrid(event_struct):
+    return
+
+
+def ui_eventSliderZ2SGT(event_struct):
+    return
+
+
+def ui_eventDropdownZ2Mode(event_struct):
+    return
+
+
+def ui_eventSliderZ3Current(event_struct):
+    return
+
+
+def ui_eventDropdownZ3Microsteps(event_struct):
+    return
+
+
+def ui_eventSliderZ3Hybrid(event_struct):
+    return
+
+
+def ui_eventSliderZ3SGT(event_struct):
+    return
+
+
+def ui_eventDropdownZ3Mode(event_struct):
+    return
+
+
+def ui_eventSliderE0Current(event_struct):
+    return
+
+
+def ui_eventDropdownE0Microsteps(event_struct):
+    return
+
+
+def ui_eventSliderE0Hybrid(event_struct):
+    return
+
+
+def ui_eventSliderE0SGT(event_struct):
+    return
+
+
+def ui_eventDropdownE0Mode(event_struct):
+    return
+
+
+def ui_eventSwitchRunout(event_struct):
+    return
+
+
+def ui_eventBtnRunoutTest(event_struct):
+    return
+
+
+def ui_eventSwitchJam(event_struct):
+    return
+
+
+def ui_eventBtnJamTest(event_struct):
+    return
+
+
+def ui_eventSliderJamSensitivity(event_struct):
+    return
+
+
+def ui_eventBtnApplySensors(event_struct):
+    return
+
+
+def ui_eventBtnSaveSensors(event_struct):
+    return
+
+
+def ui_eventBtnRefreshSensors(event_struct):
+    return
+
+
+def ui_eventFileItemClicked(event_struct):
+    return
+
+
+def ui_eventTxtXStepsClicked(event_struct):
+    return
+
+
+def ui_eventTxtYStepsClicked(event_struct):
+    return
+
+
+def ui_eventTxtZStepsClicked(event_struct):
+    return
+
+
+def ui_eventTxtE0StepsClicked(event_struct):
+    return
+
+
+def ui_eventBtnApplySteps(event_struct):
+    return
+
+
+def ui_eventTxtFilDiaClicked(event_struct):
+    return
+
+
+def ui_eventSliderFlow(event_struct):
+    return
+
+
+def ui_eventSliderBrightness(event_struct):
+    return
+
+
+def ui_eventBtnSaveAll(event_struct):
+    return
+
+
+def ui_eventBtnReboot(event_struct):
+    return
+
+
+def ui_eventNumPadDigit(event_struct):
+    return
+
+
+def ui_eventNumPadEnter(event_struct):
+    return
+
+
+def ui_eventNumPadBackspace(event_struct):
+    return
+
+
+def ui_eventNumPadClear(event_struct):
+    return
+
+
+def ui_eventNumPadCancel(event_struct):
+    return
+
+
+def ui_eventBtnCheckUpdate(event_struct):
+    return
+
+
+def ui_eventBtnShowLogs(event_struct):
+    return
+
+
+def ui_eventBtnFactoryReset(event_struct):
+    return
+
+
+def ui_eventArcHotend(event_struct):
+    return
+
+
+def ui_eventArcBed(event_struct):
+    return
+
+
+def ui_eventLabelBedValue(event_struct):
+    return
+
+
+def ui_eventBtnPLA(event_struct):
+    return
+
+
+def ui_eventBtnPETG(event_struct):
+    return
+
+
+def ui_eventBtnABS(event_struct):
+    return
+
+
+def ui_eventBtnCustom(event_struct):
+    return
+
